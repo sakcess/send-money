c2478e8935c88935b54e7051ebeedeea7872f7aa
Satish Kris <skris232@gmail.com>
2016-07-02 16:33:59 +1000

    controller_testing

--- app/main.js
+++ app/main.js
@@ -1,10 +1,22 @@
 var starter = angular.module('starter', []);
 
-starter.service('contactsService', function($http) {
-  this.contacts = [];
-  $http.get('http://localhost:9001/contacts', function(result) {
-    console.log(JSON.stringify(res));
-  });
+starter.service('ContactsService', function($http) {
+
+  var ContactService = this;
+  ContactService.contacts = [];
+
+  $http.get('http://localhost:9001/contacts')
+    .then(function(res) {
+      console.log(res);
+      while (res.data[0]) {
+        ContactService.contacts.push(res.data.pop());
+      }
+    }, function(error) {
+      console.log(error);
+    });
 });
 
 
+starter.controller('ContactController', function($scope, ContactsService) {
+  $scope.contacts = ContactsService.contacts;
+});
--- test/main.spec.js
+++ test/main.spec.js
@@ -7,19 +7,43 @@ describe("The Starter App", function() {
     beforeEach(function() {
       module('starter');
       inject(function($injector) {
-        contactsService = $injector.get('contactsService');
+        ContactsService = $injector.get('ContactsService');
         $httpBackend    = $injector.get('$httpBackend');
       });
     });
 
     it('should have contacts propert, an array', function() {
-      expect(contactsService.contacts).to.be.an('array');
+      expect(ContactsService.contacts).to.be.an('array');
     });
 
     it('should call the backend endpoint', function() {
-        $httpBackend.expectGET('http://localhost:9001/contacts').respond(200);
+        $httpBackend.expectGET('http://localhost:9001/contacts').respond(200, []);
         $httpBackend.flush();
     });
 
   });
+
+  describe('The Contact Controller', function() {
+
+    beforeEach(function() {
+      module('starter');
+      inject(function($injector, $rootScope) {
+        $scope          = $rootScope.$new();
+        ContactsService = $injector.get('ContactsService');
+        $httpBackend    = $injector.get('$httpBackend');
+        $controller     = $injector.get('$controller');
+      });
+    });
+
+    it('it should store an array of contacts in scope', function() {
+        $controller('ContactController', {
+          $scope            : $scope,
+          ContactsService   : ContactsService
+        });
+        assert.isArray($scope.contacts);
+    });
+  });
+
 });
+
+
